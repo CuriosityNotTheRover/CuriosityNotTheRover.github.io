3fe2a887b116ed4e620c669051f7e5aeca8e6256
CuriosityNotTheRover <116574839+CuriosityNotTheRover@users.noreply.github.com>
2022-11-12 08:56:23 +0100

    .

--- JS/prova.js
+++ JS/prova.js
@@ -1,3 +1,3 @@
-let text = "Prova Read";
-let result = text.link("https://www.w3schools.com");
+var text = "Prova Read";
+var result = text.link("https://www.w3schools.com");
 document.getElementById("readButton").innerHTML = result;
