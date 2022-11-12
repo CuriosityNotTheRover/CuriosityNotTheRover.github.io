d6e6628ba67bf2f0eb3dc1c0bf039722fee8b460
CuriosityNotTheRover <116574839+CuriosityNotTheRover@users.noreply.github.com>
2022-11-12 10:47:14 +0100

    First try at using JS

--- JS/prova.js
+++ JS/prova.js
@@ -1,3 +1,9 @@
 var text = "Prova Read";
 var result = text.link("https://www.w3schools.com");
 document.getElementById("readButton").innerHTML = result;
+
+var textLinkRead = "Read";
+document.getElementById("read").innerHTML = textLinkRead;
+
+var textLinkComing = "Coming Next";
+document.getElementById("coming").innerHTML = textLinkComing;
