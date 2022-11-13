85d632303f5f9438d69e81b45f620b6af140f6fc
CuriosityNotTheRover <116574839+CuriosityNotTheRover@users.noreply.github.com>
2022-11-13 15:45:06 +0100

    .

--- JS/posts.js
+++ JS/posts.js
@@ -1,4 +1,4 @@
-var website = "http://www.curiositynottherover.com" //Hosting website
+//var website = "http://www.curiositynottherover.com" //Hosting website
 const postDirectory = "Posts" // Directory of Posts
 
 
