7ff8da93f61d9e3104f373b57be69914c59ce99a
CuriosityNotTheRover <116574839+CuriosityNotTheRover@users.noreply.github.com>
2022-11-13 13:39:54 +0100

    Organizzazione dei file JS

---
+++ JS/posts.js
@@ -0,0 +1,43 @@
+/*
+var text = "Prova Read";
+var result = text.link("https://www.w3schools.com");
+document.getElementById("readButton").innerHTML = result;
+*/
+
+/*
+var textLinkRead = "Read";
+document.getElementById("read").innerHTML = textLinkRead;
+
+var textLinkComing = "Coming Next";
+document.getElementById("coming").innerHTML = textLinkComing;
+*/
+
+
+const website = "http://www.curiositynottherover.com" //Hosting website
+const postDirectory = "Posts" // Directory of Posts
+
+
+// Post to Publish
+const postToP = [   'Milestoning',
+                    'Prepare in Advance'
+];
+// Post Published
+const post = [  'An Antilibrary',
+                'Which Are My Core Values?',
+                'Passion From Jony Ive',
+                'How Writing Helps Me Think'
+];
+// Array of Posts Teaser
+const postTeasers = [
+                'Having an antilibrary is a daily reminder that you still have to learn, you’re not finished and you have not learned everything',
+                'The first question was: “Which are my core values?”. I feel like I have some values but it is curious to see how you are perceived from the outside',
+                'Jony Ive does quite a few interesting things to express his passion, I analyze the ones I find most interesting',
+                'writing down ideas helps me with the thinking process'
+];
+// Link of each post
+const linkToPosts = [
+                'antilibraryArticle.html',
+                'whichAreMyCoreValues.html',
+                'passionFromJonyIve.html',
+                'howWritingHelpsMeThink.html'
+];
