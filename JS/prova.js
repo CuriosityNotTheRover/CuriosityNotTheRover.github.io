9522287e32851ae32ccd96acbde5511087ce730f
CuriosityNotTheRover <116574839+CuriosityNotTheRover@users.noreply.github.com>
2022-11-13 13:25:35 +0100

    .

--- JS/prova.js
+++ JS/prova.js
@@ -12,32 +12,42 @@ var textLinkComing = "Coming Next";
 document.getElementById("coming").innerHTML = textLinkComing;
 */
 
+//Hosting website
+const website = "http://www.curiositynottherover.com"
+
 // Sections of the Navbar
 const sections = ['Blog', 'Learning Journal', 'Antilibrary', 'About Me'];
 // Links of the section in the Navbar
-const sectionsLinks = [ "http://www.curiositynottherover.com",
-                        "http://www.curiositynottherover.com/learningJournal.html",
-                        "http://www.curiositynottherover.com/antilibrary.html",
-                        "http://www.curiositynottherover.com/about.html",
+const sectionsLinks = [ "",
+                        "learningJournal.html",
+                        "antilibrary.html",
+                        "about.html",
 ]
 
 // Constructing the Navbar
 var text = '';
 let i = 0
 for (i = 0; i < sections.length; i++) {
-    text += "<a href=" + sectionsLinks[i] + ">" + sections[i] + "</a>"
+    text += "<a href=" + website + "/" + sectionsLinks[i] + ">" + sections[i] + "</a>"
 }
 document.getElementById("nav").innerHTML = text;
 
 
+// Directory of Posts
+const postDirectory = "Posts"
 // Post to Publish
 const postToP = [   'Milestoning',
                     'Prepare in Advance'
 ];
 // Post Published
-const post = [  'Blog', 'Learning Journal', 'Antilibrary',
-                'How Writing Helps Me Think'];
-
+const post = [  'An Antilibrary',
+                'Which Are My Core Values?',
+                'Passion From Jony Ive',
+                'How Writing Helps Me Think'
+];
+const linkToPosts = [
+                'prova'
+];
 
 
 /*var data = $.csv.toObjects(csv)*/
