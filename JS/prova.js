8811183b06a48aa7949c8ad591d7511ef4fba072
CuriosityNotTheRover <116574839+CuriosityNotTheRover@users.noreply.github.com>
2022-11-13 12:34:32 +0100

    .

--- JS/prova.js
+++ JS/prova.js
@@ -12,18 +12,23 @@ var textLinkComing = "Coming Next";
 document.getElementById("coming").innerHTML = textLinkComing;
 */
 
-const sections = ['Blog', 'Learning Journal', 'Antilibrary', 'About Me', 'prova'];
+// Sections of the Navbar
+const sections = ['Blog', 'Learning Journal', 'Antilibrary', 'About Me'];
+// Links of the section in the Navbar
 const sectionsLinks = [ "http://www.curiositynottherover.com",
                         "http://www.curiositynottherover.com/learningJournal.html",
                         "http://www.curiositynottherover.com/antilibrary.html",
                         "http://www.curiositynottherover.com/about.html",
-                        "#"]
+]
+
+// Constructing the Navbar
 var text = '';
 var temp;
 var result
-for (let i = 0; i < sections.length; i++) {
+let i = 0
+for (i = 0; i < sections.length; i++) {
     temp = sections[i];
-    result = text.link(sectionsLinks[i]);
+    result = temp.link(sectionsLinks[i]);
     text += result;
 }
 document.getElementById("nav").innerHTML = text;
