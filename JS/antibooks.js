51f2ab392800236c36003e5283a7fb6f0bc0cf2f
CuriosityNotTheRover <giulio.111213@gmail.com>
2022-11-22 22:20:10 +0100

    .

---
+++ JS/antibooks.js
@@ -0,0 +1,28 @@
+//var website = "http://www.curiositynottherover.com" //Hosting website
+
+
+// Post Published [title, [author/s]]
+const books = [ ['The Black Swan', ['Nassim Nicholas Taleb']],
+                ['Overcomplicated', ['Samuel Arbesman']],
+                ['All Art is Propaganda', ['George Orwell']],
+                ['The Tools of Argument', ['Joel P. Trachtman']]
+];
+// Array containing the images   TO UPDATE
+const images = [
+                'Having an antilibrary is a daily reminder that you still have to learn, you’re not finished and you have not learned everything',
+                'The first question was: “Which are my core values?”. I feel like I have some values but it is curious to see how you are perceived from the outside',
+                'Jony Ive does quite a few interesting things to express his passion, I analyze the ones I find most interesting',
+                'writing down ideas helps me with the thinking process'
+];
+
+// Constructing the cards with the books
+var text = '';
+var j = 0
+var i = 0
+for (j = 0; j < books.length; j++) {
+    text += '<div class="card"><h4><b>' + books[j] + '</b></h4><p>'
+    for (i = 0; i < books[1].length; i++) {
+        text += books[1][i] + '</p></div>'
+    }
+}
+document.getElementById("books").innerHTML = text;
