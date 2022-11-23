63b21db2fc415da2e3ab247b198e67fc90156d1d
CuriosityNotTheRover <giulio.111213@gmail.com>
2022-11-23 22:58:04 +0100

    major update to the antilibrary cards

--- JS/antibooks.js
+++ JS/antibooks.js
@@ -16,15 +16,30 @@ const images = [
                 'TheToolsOfArgument-JoelTrachtman.png'
 ];
 
+/*
+    <div class="books">    
+		<div class="bookCover">
+			<img class="cover" src="http://www.curiositynottherover.com/img/BooksCovers/TheBlackSwan-NassimNicholasTaleb.jpg">
+		</div>
+		<div class="content">
+			<p class="booktitle"><h4><b>Titolo</b></h4></p>
+			<p class="bookauthor">Autore</p>
+		</div>
+	</div>
+*/
+
 // Constructing the cards with the books.
 var text = '';
 var j = 0
 var i = 0
 for (j = 0; j < books.length; j++) {
-    text += '<div class="card"><h4><b>' + books[j][0] + '</b></h4><p>'
+    text += '<div class="books"><div class="bookCover">' + 
+        '<img class="cover" src="' +  website + "/" + coverDirectory + images[j] + '"></div>' +
+        '<div class="content"><p class="booktitle"><h4><b>' + books[j][0] + '</b></h4><p>' +
+        '<p class="bookauthor">'
     for (i = 0; i < books[j][1].length; i++) {
         text += books[j][1][i] + ', '
     }
-    text += '</p><br><img src=' + website + "/" + coverDirectory + images[j] + ' width="100px">' + '</div>'
+    text += '</p></div></div>'
 }
 document.getElementById("books").innerHTML = text;
